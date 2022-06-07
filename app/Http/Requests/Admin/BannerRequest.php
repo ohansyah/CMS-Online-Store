2b5b6395bec14ae5a65bd3635eade5239fb43b45
ohansyah <ekiohansyah@yahoo.co.id>
2022-06-07 10:14:52 +0700

    refactor:banner create and update - form request - helper

---
+++ app/Http/Requests/Admin/BannerRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class BannerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'type' => 'required',
+            'start_date' => 'required',
+            'end_date' => 'required',
+            'image' => 'image|nullable|max:4000',
+        ];
+    }
+}
