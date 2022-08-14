6f3cdc0d19a54498e4d3327059a6013179cbba1b
ohansyah <ekiohansyah@yahoo.co.id>
2022-08-14 18:46:20 +0700

    perf: eager loading datatable products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -30,7 +30,7 @@ class Product extends Model
 
     public function category()
     {
-        return $this->belongsTo('App\Models\Category', 'category_id');
+        return $this->belongsTo(Category::class, 'category_id');
     }
 
     public function productImages()
