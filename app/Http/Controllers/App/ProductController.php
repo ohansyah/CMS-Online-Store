5ce4f6cbb6b43e4f915d9ddbc1ffb9b02b2b5965
ohansyah <ekiohansyah@yahoo.co.id>
2022-10-05 22:47:38 +0700

    remove: unused query

--- app/Http/Controllers/App/ProductController.php
+++ app/Http/Controllers/App/ProductController.php
@@ -11,11 +11,9 @@ class ProductController extends Controller
 {
     public function index(Request $request)
     {
-        $wa_link = GeneralSetting::where('name', 'wa_link')->first()->value;
         $products = Product::search($request)->with(['category', 'productImages'])->sort($request)->paginate(12);
         $products->appends(['category_id' => $request->category_id]);
         return view('app.product.index')
-            ->with('wa_link', $wa_link)
             ->with('products', $products);
     }
 
