366919c604e6fdb9be605d3f8d2765a80c1b0819
ohansyah <ekiohansyah@yahoo.co.id>
2022-12-30 13:51:20 +0700

    feat: search product

--- resources/views/inc/app/header.blade.php
+++ resources/views/inc/app/header.blade.php
@@ -4,7 +4,7 @@
         <nav class="navbar navbar-expand-lg navbar-light main_box">
             <div class="container">
                 <!-- Brand and toggle get grouped for better mobile display -->
-                <a class="navbar-brand logo_h" href="{{route('app.home.index')}}"><img src="{{ asset('karma/img/logo.png') }}"
+                <a class="navbar-brand logo_h" href="{{route('app.home.index')}}#"><img src="{{ asset('karma/img/logo.png') }}"
                         alt=""></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -32,8 +32,8 @@
     </div>
     <div class="search_input" id="search_input_box">
         <div class="container">
-            <form class="d-flex justify-content-between">
-                <input type="text" class="form-control" id="search_input" placeholder="Search Here">
+            <form class="d-flex justify-content-between" method="GET" action="/product?search=" >
+                <input type="text" class="form-control" id="search_input" name="search" placeholder="Search Here">
                 <button type="submit" class="btn"></button>
                 <span class="lnr lnr-cross" id="close_search" title="Close Search"></span>
             </form>
