468ce3f740a06b19562de122029ac842f4c03b4e
ohansyah <ekiohansyah@yahoo.co.id>
2023-01-28 22:12:23 +0700

    feat: customize content Hotdeals from cms

--- app/Http/Controllers/App/HomeController.php
+++ app/Http/Controllers/App/HomeController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Banner;
 use App\Models\Category;
 use App\Models\GeneralSetting;
+use App\Models\HotDeal;
 use App\Models\Product;
 
 class HomeController extends Controller
@@ -33,6 +34,19 @@ class HomeController extends Controller
         $products = Product::with(['category', 'productImages'])->orderBy('id', 'desc')->limit(16)->get();
         $jerseys = Product::with(['category', 'productImages'])->where('category_id', 7)->limit(4)->get();
         $dealOfTheWeeks = Product::with(['category', 'productImages'])->inRandomOrder()->limit(6)->get();
+        
+        $hotDeals = HotDeal::active()->limit(4)->get();
+        foreach ($hotDeals as $hotDeal) {
+            $query = Product::with(['category', 'productImages']);
+            if ($hotDeal->type == 'product-list') {
+                $query->whereIn('id', $hotDeal->data->product_ids);
+            } else {
+                $query->where('category_id', $hotDeal->data->category_id)
+                    ->orderBy('id', 'desc')
+                    ->limit($hotDeal->data->product_total);
+            }
+            $hotDeal->products = $query->get();
+        }
 
         return view('app.index')
             ->with('wa_link', $wa_link)
@@ -42,6 +56,7 @@ class HomeController extends Controller
             ->with('latestProducts', $products->slice(0, 8))
             ->with('comingProducts', $products->slice(8, 8))
             ->with('jerseys', $jerseys)
-            ->with('dealOfTheWeeks', $dealOfTheWeeks);
+            ->with('dealOfTheWeeks', $dealOfTheWeeks)
+            ->with('hotDeals', $hotDeals);
     }
 }
